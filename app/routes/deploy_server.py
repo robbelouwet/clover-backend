91f05262e999d35d9ba932175cdbcf53cabb5a72
Robbe Louwet <robbe.louwet@nara.to>
2024-02-18 15:12:14 +0100

    fixed packages

--- app/routes/deploy_server.py
+++ app/routes/deploy_server.py
@@ -19,12 +19,12 @@ allowed_values = [[0.5, 1], [1, 2], [1.5, 3], [2, 4]]
 @deploy_server.route('/deploy-dedicated')
 @cross_origin(supports_credentials=True)
 def deploy_dedicated():
-    logging.info(f"x-ms-client-principal: {request.headers.get('x-ms-client-principal')}")
-    # Validate google service principal authentication
-    client_principal = json.loads(base64.b64decode(request.headers.get('x-ms-client-principal')))
-    google_name_identifier = parse_principal_name_identifier(client_principal)
-
-    logging.info(f"google_nameidentifier: {google_name_identifier}")
+    # logging.info(f"x-ms-client-principal: {request.headers.get('x-ms-client-principal')}")
+    # # Validate google service principal authentication
+    # client_principal = json.loads(base64.b64decode(request.headers.get('x-ms-client-principal')))
+    # google_name_identifier = parse_principal_name_identifier(client_principal)
+    #
+    # logging.info(f"google_nameidentifier: {google_name_identifier}")
     # if not google_name_identifier: return jsonify({}), 401
 
     # Parse & set up deployment params
